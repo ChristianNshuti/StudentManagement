e1ec17c059179a93c6876172d729493b0bac822d
christiannshuti <chrisnshuti943@gmail.com>
2026-03-04 07:51:30 +0200

    Filled the Student entity

---
+++ src/main/java/student/entity/Student.java
@@ -0,0 +1,35 @@
+package student.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private int age;
+
+    public Student() {}
+
+    public Student(String name,String email,int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
