26e3208eba10f40f75bd58ad11c78d6b41b5b33f
christiannshuti <chrisnshuti943@gmail.com>
2026-03-04 07:55:57 +0200

    Added and filled Hibernate Util

---
+++ src/main/java/student/util/HibernateUtil.java
@@ -0,0 +1,14 @@
+package student.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    public static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
